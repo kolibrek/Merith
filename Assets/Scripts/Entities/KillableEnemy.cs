70ef7e3f221f46f82eac712804bd8017e6eec36f
Kevin Clemons <10484164@CS518-03.ad.uvu.edu>
2016-02-25 09:50:52 -0700

    Added basic smashing enemy function.

---
+++ Assets/Scripts/Entities/KillableEnemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Controller2D))]
+public class KillableEnemy : MonoBehaviour {
+
+	Controller2D controller;
+	GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (controller.status.above) {
+			foreach (GameObject collider in controller.status.colliders) {
+				if (collider.GetComponent<Controller2D>()) {
+					collider.GetComponent<Controller2D>().status.colliders.Remove(gameObject);
+					Die ();
+					break;
+				}
+			}
+		} 
+	}
+
+	void Die() {
+		Destroy (gameObject);
+	}
+}
