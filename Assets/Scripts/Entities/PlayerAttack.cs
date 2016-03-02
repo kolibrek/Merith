2ae30c8441469943f4956b8cadb52c8801883536
Kevin Clemons <kolibermedia@gmail.com>
2016-03-02 08:04:52 -0700

    Added Attack sprite animation

--- Assets/Scripts/Entities/PlayerAttack.cs
+++ Assets/Scripts/Entities/PlayerAttack.cs
@@ -13,16 +13,6 @@ public class PlayerAttack : MonoBehaviour {
 	void Start () {
 		Invoke ("Fade", time);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		//transform.Rotate(Vector3.forward * speed);
-		//Quaternion rotate = Quaternion.Euler(0,0,speed);
-		//transform.localRotation *= rotate;
-		if (transform.localPosition.y > -1f) {
-			transform.Translate(new Vector3(0, -speed, 0));
-		}
-	}
 
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.GetComponent<HealthController>()) {
